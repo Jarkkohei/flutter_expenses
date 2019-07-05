53e8490b96f5555541a0800dec2569b6bc2699fd
Jarkko <jarkkohei@luukku.com>
2019-07-05 16:57:38 +0300

    Add currency in front of amount

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,8 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        '€ ${tx.amount}',
+                        //'\$ ${tx.amount}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
