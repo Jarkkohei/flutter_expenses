80552b5f43ccfd916457259b156eba34d6df68be
Jarkko <jarkkohei@luukku.com>
2019-07-07 02:53:00 +0300

    Add UI and implement functionality for deleting transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -5,8 +5,9 @@ import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final Function deleteTx;
 
-  TransactionList(this.transactions);
+  TransactionList(this.transactions, this.deleteTx);
 
   @override
   Widget build(BuildContext context) {
@@ -55,6 +56,13 @@ class TransactionList extends StatelessWidget {
                       DateFormat.yMMMd().format(transactions[index].date),
                       style: TextStyle(
                         color: Colors.grey,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () => deleteTx(transactions[index].id),
+                      color: Theme.of(context).errorColor,
+                      icon: Icon(
+                        Icons.delete,
                       ),
                     ),
                   ),
