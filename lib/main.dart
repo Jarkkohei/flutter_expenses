dc4c339bf477b260e808d8853f67653cfddce98f
Jarkko <jarkkohei@luukku.com>
2019-07-06 20:13:52 +0300

    Change the app title

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Expenses',
       home: MyHomePage(),
     );
   }
@@ -68,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Expenses'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
